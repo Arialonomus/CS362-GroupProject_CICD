ee8499195c75e0c2581c2735ca470f52f402c964
Jacob Barber <barber.jacobervin@gmail.com>
2024-05-29 13:57:36 -0500

    Added code to strip the negative character from the string and flag string as negative, modified regex to no longer allow negative symbols, passes test5

--- task.py
+++ task.py
@@ -7,15 +7,23 @@ def conv_num(num_str):
         return None
 
     # Validate input string is not empty
-    if len(num_str) < 1:
+    str_len = len(num_str)
+    if str_len < 1:
+        return None
+
+    # Strip negative symbol if present
+    is_negative = False
+    if num_str[0] == '-':
+        is_negative = True
+        num_str = num_str[1:]
+        str_len -= 1
+
+    # Validate input string contains no invalid non-numeric characters
+    if re.search(r'[^0-9.]', num_str) is not None:
         return None
 
     # Validate input string contains no more than one decimal point
     if num_str.count('.') > 1:
-        return None
-
-    # Validate input string contains no invalid non-numeric characters
-    if re.search(r'[^0-9.-]', num_str) is not None:
         return None
 
     return False
