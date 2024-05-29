f8347d1383526589aae72a9ffecb8cfbc954a4b4
Jacob Barber <barber.jacobervin@gmail.com>
2024-05-29 13:31:07 -0500

    Added test3 to check if function returns None for strings with multiple decimals

--- tests.py
+++ tests.py
@@ -3,15 +3,21 @@ from task import conv_num
 
 
 class TestConvNum(unittest.TestCase):
-    """Test that the function returns None for non-string values"""
+    """Test that conv_num returns None for non-string values"""
     def test1(self):
         test_case = 1
         expected_val = None
         self.assertEqual(conv_num(test_case), expected_val)
 
-    """Test that the function returns None for the empty string"""
+    """Test that conv_num returns None for the empty string"""
     def test2(self):
-        test_case = ""
+        test_case = ''
+        expected_val = None
+        self.assertEqual(conv_num(test_case), expected_val)
+
+    """Test that conv_num returns None for strings with multiple decimals"""
+    def test3(self):
+        test_case = '12.3.45'
         expected_val = None
         self.assertEqual(conv_num(test_case), expected_val)
 
