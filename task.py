9bdb736ebd3f3c4438b52cd5063127d7895a12f0
Jacob Barber <barber.jacobervin@gmail.com>
2024-05-29 13:45:07 -0500

    Added code to validate that input string contains no invalid non-numeric characters, passes test4

--- task.py
+++ task.py
@@ -1,3 +1,6 @@
+import re
+
+
 def conv_num(num_str):
     # Validate input is of type str
     if type(num_str) is not str:
@@ -9,6 +12,10 @@ def conv_num(num_str):
 
     # Validate input string contains no more than one decimal point
     if num_str.count('.') > 1:
+        return None
+
+    # Validate input string contains no invalid non-numeric characters
+    if re.search(r'[^0-9.-]', num_str) is not None:
         return None
 
     return False
